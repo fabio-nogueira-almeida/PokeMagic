1ec9965c70aab03ee844f96de0cdce35a73090e4
Fábio Nogueira de Almeida <fna.contact@gmail.com>
2015-07-18 15:29:22 -0300

    Adicionando classe de modelo

---
+++ PokeMagic/Pokemon.swift
@@ -0,0 +1,16 @@
+//
+//  Pokemon.swift
+//  PokeMagic
+//
+//  Created by Fábio Nogueira de Almeida on 18/07/15.
+//  Copyright (c) 2015 Fábio Nogueira de Almeida. All rights reserved.
+//
+
+import UIKit
+
+class Pokemon: NSObject {
+ 
+    var nome :String!
+    var apelido :String!
+    
+}
